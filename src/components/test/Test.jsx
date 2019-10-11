4cb0c829da29fc37def7dd917317ff37488a5280
Daniel Fornells <daniel.garcia.fornells@gmail.com>
2019-10-12 00:29:13 +0200

    Basic React & Storybook setup

---
+++ src/components/test/Test.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Test = ({children, ...props}) => (
+  <h1>{children}</h1>
+);
+
+export default Test;
---
+++ src/components/test/Test.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Test from './Test';
+
+export default { title: 'Components|Test' };
+
+export const myFirstTest = () => <Test>Hello World</Test>;
+
+export const mySecondTest = () => <Test>Bye World</Test>;
