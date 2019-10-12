c7eb79abf08dc0999f0f58f2d7898d7a1c8fb7dd
Daniel Fornells <daniel.garcia.fornells@gmail.com>
2019-10-13 00:39:08 +0200

    Enable SASS compilation

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+// Extend Storybook webpack configuration
+module.exports = async ({ config, mode }) => {
+
+  // Configure css-loader to disable CSS modules transformations
+  const customCssLoader = {
+    loader: 'css-loader',
+    options: {
+      modules: {
+        mode: 'local',
+        localIdentName: '[local]'
+      }
+    }
+  };
+
+  // Enable styles processing (mainly SASS, POSTCSS)
+  config.module.rules.push({
+    test: /\.scss$/,
+    use: ['style-loader', customCssLoader, 'postcss-loader', 'sass-loader'],
+    include: path.resolve(__dirname, '../'),
+  });
+
+  return config;
+};
--- src/components/test/Test.jsx
+++ src/components/test/Test.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import STYLES from './Test.scss';
+
+const c = className => STYLES[className] || 'UNKNOWN';
 
 const Test = ({children, ...props}) => (
-  <h1>{children}</h1>
+  <h1 className={c('Test')}>{children}</h1>
 );
 
 export default Test;
