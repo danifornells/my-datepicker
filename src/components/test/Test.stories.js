7a70bac51b7584454940aaf62b43c7366c91b2f2
Daniel Fornells <daniel.garcia.fornells@gmail.com>
2019-10-20 02:03:43 +0200

    Delete test component

--- src/components/test/Test.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Test from './Test';
-import IconClose from '../icons/IconClose'
-
-export default { title: 'Components|Test' };
-
-export const myFirstTest = () => <Test>Hello World <IconClose></IconClose></Test>;
-
-export const mySecondTest = () => <Test>Bye World</Test>;
